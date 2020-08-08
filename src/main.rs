d555ab137f48784d29c135a98d0f00b0e2208d5a
Diana <5275194+DianaNites@users.noreply.github.com>
2020-08-08 13:17:22 -0400

    Very simple

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 #![no_std]
 #![no_main]
 #![feature(abi_efiapi)]
-use log::info;
+use log::*;
 use uefi::prelude::*;
 
 #[entry]
 fn efi_main(_img: Handle, st: SystemTable<Boot>) -> Status {
     uefi_services::init(&st).expect_success("Failed to init");
-
     let rev = st.uefi_revision();
+    let stdout = st.stdout();
+    trace!("Started!");
+    info!("UEFI {:?}", rev);
+    stdout.reset(false).unwrap_success();
+    trace!("Cleared console!");
+
+    let mode = stdout.modes().last().unwrap().log();
+    info!("Setting output mode to: {:?}", mode);
+    stdout.set_mode(mode).log_warning().unwrap();
+    info!("Set output mode to: {:?}", mode);
     info!("UEFI {:?}", rev);
 
-    info!("{:?}", st.stdout().current_mode());
-
-    Status::SUCCESS
+    loop {}
 }
