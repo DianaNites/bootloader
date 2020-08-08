226da636e909ee0ad9a6756163689bde08a037da
Diana <5275194+DianaNites@users.noreply.github.com>
2020-08-08 03:53:36 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(abi_efiapi)]
+use core::panic::PanicInfo;
+use uefi::prelude::*;
+
+#[entry]
+fn efi_main(_handle: Handle, _system_table: SystemTable<Boot>) -> Status {
+    todo!()
+}
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    todo!()
 }
