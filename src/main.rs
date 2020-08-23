abd60e8cfb141e493cbef00f3044c7e7aedfc2c4
Diana <5275194+DianaNites@users.noreply.github.com>
2020-08-22 20:59:12 -0400

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_variables)]
 #![no_std]
 #![no_main]
 #![feature(abi_efiapi)]
@@ -9,9 +10,16 @@ fn efi_main(_img: Handle, st: SystemTable<Boot>) -> Status {
     fn res() -> (usize, usize) {
         (1280u32 as usize, 768u32 as usize)
     }
+    // This will lead to a crash? thing? qemu will infinitely reset.
     let (display_width, display_height) = res();
-    let (_display_width, _display_height) =
-        ((display_width / 2) as f32, (display_height / 2) as f32);
+
+    // This will not.
+    // let (display_width, display_height) = (1280u32 as usize, 768u32 as usize);
+
+    // The actual crash will only happen if this is here.
+    let (display_width, display_height) = ((display_width / 2) as f64, (display_height / 2) as f64);
+
+    // Loop forever and also do something.
     loop {
         st.boot_services().stall(1000)
     }
@@ -20,6 +28,7 @@ fn efi_main(_img: Handle, st: SystemTable<Boot>) -> Status {
 #[panic_handler]
 fn panic_handler(_: &core::panic::PanicInfo) -> ! {
     let mut x = 0;
+    // Loop forever and also do something.
     loop {
         x += 1;
         if x == !0 {
